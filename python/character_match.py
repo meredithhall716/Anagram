a7a2b667529afd39a89eef25e0a0f389d1b8b317
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-05-30 20:23:00 -0400

    first

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,17 @@
-# Don't forget to run your tests!
-
 def is_character_match(string1, string2):
-	pass
-	# Your code here
+	lower_string1 = str(string1.lower().replace(" ", ""))
+	lower_string2 = str(string2.lower().replace(" ", ""))
+
+	split_string1 = list(lower_string1)
+	split_string2 = list(lower_string2)
+
+	sorted_string1 = sorted(split_string1)
+	sorted_string2 = sorted(split_string2)
+
+	
+	if sorted_string1 == sorted_string2:
+		return True
+	else:
+		 return False
+
+is_character_match('Anna Madrigal', 'A man and a girl')
